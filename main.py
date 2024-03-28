94f6ce056fbeaf6c5707ffbed288765d4f824396
wilfried <wilfried.odorico@gmail.com>
2024-03-28 13:02:03 +0100

    book page retrieval with title, description and category

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("hello")
+import requests
+from bs4 import BeautifulSoup
+
+url = "http://books.toscrape.com/"
+url_business_book = url + "catalogue/the-dirty-little-secrets-of-getting-your-dream-job_994/index.html"
+
+response = requests.get(url_business_book)
+
+if response.ok:
+    soup = BeautifulSoup(response.content, features="html.parser")
+
+    title = soup.find("h1").text.strip()
+    
+    description = soup.find("div", id="product_description").find_next_sibling("p").text.strip()
+    
+    category = soup.find("ul", "breadcrumb").find_all("a")[2].text.strip()
+
+
